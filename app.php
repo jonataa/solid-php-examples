27fa1a24c7a9f03f11230966ae411238e6e28064
Jonata Weber <jonataa@gmail.com>
2018-09-11 10:08:18 -0300

    before refactory

--- app.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$areas = new AreaCalculator($shapes);
-$volumes = new AreaCalculator($solidShapes);
-
-$output = new SumCalculatorOutputter($areas);
-$output2 = new SumCalculatorOutputter($volumes);
---
+++ src/PasswordReminder.php
@@ -0,0 +1,11 @@
+<?php
+
+class PasswordReminder 
+{
+    private $dbConnection;
+
+    public function __construct(MySQLConnection $dbConnection) 
+    {
+        $this->dbConnection = $dbConnection;
+    }
+}
