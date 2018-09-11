c8b0be281251b8915cc2c682dcbcce974ea2c568
Jonata Weber <jonataa@gmail.com>
2018-09-11 10:09:20 -0300

    after refactory

--- src/PasswordReminder.php
+++ src/PasswordReminder.php
@@ -1,11 +1,24 @@
 <?php
 
+interface DBConnectionInterface 
+{
+    public function connect();
+} 
+
+class MySQLConnection implements DBConnectionInterface 
+{
+    public function connect() 
+    {
+        return "Database connection";
+    }
+}
+
 class PasswordReminder 
 {
     private $dbConnection;
 
-    public function __construct(MySQLConnection $dbConnection) 
+    public function __construct(DBConnectionInterface $dbConnection) 
     {
         $this->dbConnection = $dbConnection;
     }
-}
+} 
