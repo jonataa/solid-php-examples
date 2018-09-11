affad1b6d623eb5cec85616e29078d5913d2c2d6
Jonata Weber <jonataa@gmail.com>
2018-09-11 09:51:07 -0300

    after refactory

--- src/AreaCalculator.php
+++ src/AreaCalculator.php
@@ -6,18 +6,19 @@ class AreaCalculator
     protected $shapes;
 
     public function __construct($shapes = array()) 
-    {
+    {        
         $this->shapes = $shapes;
     }
 
     public function sum() 
     {
         foreach ($this->shapes as $shape) {
-            if (is_a($shape, 'Square')) {
-                $area[] = pow($shape->length, 2);
-            } else if(is_a($shape, 'Circle')) {
-                $area[] = pi() * pow($shape->radius, 2);
+            if (is_a($shape, 'ShapeInterface')) {
+                $area[] = $shape->area();
+                continue;
             }
+    
+            throw new AreaCalculatorInvalidShapeException;
         }
     
         return array_sum($area);
--- src/Objects.php
+++ src/Objects.php
@@ -1,6 +1,11 @@
 <?php
 
-class Circle 
+interface ShapeInterface 
+{
+    public function area();
+}
+
+class Circle implements ShapeInterface 
 {
     public $radius;
 
@@ -8,14 +13,24 @@ class Circle
     {
         $this->radius = $radius;
     }
-}
 
-class Square 
+    public function area() 
+    {
+        return pi() * pow($this->radius, 2);
+    }
+}  
+
+class Square implements ShapeInterface 
 {
     public $length;
 
     public function __construct($length) 
     {
         $this->length = $length;
+    }
+
+    public function area()
+    {
+        return pow($shape->length, 2);
     }
 }
